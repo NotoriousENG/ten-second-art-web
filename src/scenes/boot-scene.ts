91b40636e69211753da81a8466b99f512d1c2d4b
christina salem <christinaasalem321@gmail.com>
2022-10-02 16:11:39 -0400

    add our bg

--- src/scenes/boot-scene.ts
+++ src/scenes/boot-scene.ts
@@ -76,7 +76,7 @@ export class BootScene extends Phaser.Scene {
     // Source: Open Game Art
     this.load.image('man', 'assets/sprites/character.png');
     this.load.image('cat', 'assets/sprites/cat-900x900.png');
-    this.load.image('bg', 'assets/sprites/test_bg.jpg');
+    this.load.image('bg', 'assets/sprites/background.png');
     this.load.image('splat', 'assets/sprites/splat.jpg');
     this.load.image('easel', 'assets/sprites/easelPaper.png');
     this.load.image('water', 'assets/sprites/water.png');
