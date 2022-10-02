cee9517402408b1a1164c8d7c34c0f93aba731fa
christina salem <christinaasalem321@gmail.com>
2022-10-02 17:57:41 -0400

    cat and final bg

--- src/scenes/boot-scene.ts
+++ src/scenes/boot-scene.ts
@@ -75,7 +75,7 @@ export class BootScene extends Phaser.Scene {
 
     // Source: Open Game Art
     this.load.image('man', 'assets/sprites/character.png');
-    this.load.image('cat', 'assets/sprites/cat-900x900.png');
+    this.load.image('cat', 'assets/sprites/real-cat.png');
     this.load.image('bg', 'assets/sprites/background.png');
     this.load.image('splat', 'assets/sprites/splat.png');
     this.load.image('easel', 'assets/sprites/easelPaper.png');
